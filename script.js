6e195001d40827868ced0788b5d1222e2fa6fe38
Angelo Barbarulo <92362212+Jorahhh@users.noreply.github.com>
2022-05-18 12:52:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+document.querySelectorAll('.accordion_question').forEach((question) =>
+    question.addEventListener('click', () =>{
+
+        const questions = question;
+        const answers = question.nextElementSibling;
+
+        //Statement to add classes to the variables above;
+        if(question.classList.contains('active')){
+
+            answers.classList.toggle('open');
+            questions.classList.toggle('active');
+
+        }else {
+
+            //Avoiding to open multiple accordions at the same time
+            document.querySelectorAll('.accordion_question').forEach((question) =>
+                question.classList.remove('active'));
+            question.classList.add('active');
+
+            document.querySelectorAll('.accordion_content').forEach((content)=>
+                content.classList.remove('open'))
+            answers.classList.add('open');
+        }
+}))
